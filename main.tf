e7e7c7dd3df746b13c58e05f37ebafa79c6f982e
Fatima Jafri <fatima.jafri123@live.com>
2023-04-27 14:32:10 +0500

    Add files via upload

--- main.tf
+++ main.tf
@@ -3,32 +3,6 @@ provider "aws" {
   region = "eu-west-1"
 }
 
-#Create EC2 Instance
-resource "aws_instance" "instance1" {
-  ami                    = "ami-04f1014c8adcfa670"
-  instance_type          = "t2.micro"
-  vpc_security_group_ids = [aws_security_group.terraform_sg.id]
-  tags = {
-    Name = "terraform_instance"
-  }
-  
-  #Bootstrap APACHE installation and start
-  user_data = <<-EOF
-  #!/bin/bash
-  # Use this for your user data (script from top to bottom)
-  # install httpd (Linux 2 version)
-  sudo yum update -y
-  sudo yum install -y httpd
-  sudo systemctl start httpd
-  sudo systemctl enable httpd
-  echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
-  
-  EOF
-  
-  user_data_replace_on_change = true
-    
-}
-
 #Create security group 
 resource "aws_security_group" "terraform_sg" {
   name        = "terraform_sg"
@@ -43,7 +17,7 @@ resource "aws_security_group" "terraform_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  #Allow incoming TCP requests on port 8080 from any IP
+  #Allow incoming TCP requests on port 80 from any IP
   ingress {
     description = "Incoming 80"
     from_port   = 80
@@ -73,3 +47,30 @@ resource "aws_security_group" "terraform_sg" {
     Name = "terraform_sg"
   }
 }
+
+#Create EC2 Instance
+resource "aws_instance" "instance1" {
+  ami                    = "ami-04f1014c8adcfa670"
+  instance_type          = "t2.micro"
+  vpc_security_group_ids = [aws_security_group.terraform_sg.id]
+  tags = {
+    Name = "terraform_instance"
+  }
+  
+  #Bootstrap APACHE installation and start
+  user_data = <<-EOF
+  #!/bin/bash
+  # Use this for your user data (script from top to bottom)
+  # install httpd (Linux 2 version)
+  sudo yum update -y
+  sudo yum install -y httpd
+  sudo systemctl start httpd
+  sudo systemctl enable httpd
+  echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+  
+  EOF
+  
+  user_data_replace_on_change = true
+    
+}
+
